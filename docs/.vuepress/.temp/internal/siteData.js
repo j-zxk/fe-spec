d324891c540b9d6fc44fddbead410cef39f494f8
zxkxyz <522569695@qq.com>
2025-03-24 13:17:23 +0800

    chore: run git config --global

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,1 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"你好， VuePress ！\",\"description\":\"这是我的第一个 VuePress 站点\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"你好， VuePress ！\",\"description\":\"这是我的第一个 VuePress 站点2\",\"head\":[],\"locales\":{}}")
