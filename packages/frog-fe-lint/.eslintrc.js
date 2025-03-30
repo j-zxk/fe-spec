5fb939c4b16c8d3191f25b4e21332d84ebe409d5
zxkxyz <522569695@qq.com>
2025-03-30 14:23:03 +0800

    fix: frog-fe-lint config/*.ejs

--- packages/frog-fe-lint/.eslintrc.js
+++ packages/frog-fe-lint/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['encode-fe-eslint-config/typescript/node', 'prettier'],
+  extends: ['frog-eslint-config/typescript/node', 'prettier'],
   rules: {
     '@typescript-eslint/no-require-imports': 0,
     'no-console': 0,
